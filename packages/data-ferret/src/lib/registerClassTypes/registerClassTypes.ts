d880209ec8ef630df827a9e5270a817a64c495ae
AndrewRedican <andrew.redican.mejia@gmail.com>
2022-11-02 00:28:24 +0000

    docs(data-ferret): annotate registerClassTypes to expected behavior

--- packages/data-ferret/src/lib/registerClassTypes/registerClassTypes.ts
+++ packages/data-ferret/src/lib/registerClassTypes/registerClassTypes.ts
@@ -1,5 +1,9 @@
 import { registeredClasses } from '../shared/consts'
 
+/**
+ * Registers one or more classes which will set up the rest of the data-ferret'd API to treat instances
+ * said class or classes as having their own unique data type that corresponds to their class.
+ */
 export const registerClassTypes = (...classes: unknown[]): void => {
   const list = ([] as unknown[]).concat(classes)
   while (registeredClasses.length) registeredClasses.pop()
