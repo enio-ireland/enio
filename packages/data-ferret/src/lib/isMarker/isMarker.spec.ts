7bb8f6956d77d9bc6a5a27883a433b7a4ceced86
AndrewRedican <andrew.redican.mejia@gmail.com>
2022-11-04 00:22:43 +0000

    fix(data-ferret): ease regex that failed inconsistently on isMarker :bug:

--- packages/data-ferret/src/lib/isMarker/isMarker.spec.ts
+++ packages/data-ferret/src/lib/isMarker/isMarker.spec.ts
@@ -6,7 +6,6 @@ describe('isMarker', () => {
   })
 
   it('should return false when text does matches the expected format', () => {
-    expect(isMarker('__$9618480541570961848054157')).toEqual(false)
     expect(isMarker('_$96184805415709618480541570')).toEqual(false)
     expect(isMarker('__96184805415709618480541570')).toEqual(false)
     expect(isMarker('')).toEqual(false)
--- packages/data-ferret/src/lib/isMarker/isMarker.ts
+++ packages/data-ferret/src/lib/isMarker/isMarker.ts
@@ -1,4 +1,4 @@
 export const isMarker = (text: string): boolean => {
   if (!text || typeof text !== 'string' || text.length !== 29 || !text.includes('__$')) return false
-  return /^__\$[0-9]{26}$/.test(text)
+  return /^__\$[0-9]+$/.test(text)
 }
