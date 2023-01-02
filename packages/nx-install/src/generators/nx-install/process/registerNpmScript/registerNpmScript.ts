4c960505e1fbfc8dd5cf6e72558a78a8464d594e
Andrew Redican <andrew.redican.mejia@gmail.com>
2023-01-02 05:46:57 +0000

    refactor(nx-install): update nx-install script

--- packages/nx-install/src/generators/nx-install/process/registerNpmScript/registerNpmScript.ts
+++ packages/nx-install/src/generators/nx-install/process/registerNpmScript/registerNpmScript.ts
@@ -4,6 +4,6 @@ import { readRootPackageJson, workspaceRoot, writeJsonFile } from '@nrwl/devkit'
 export const registerNpmScript = (): void => {
   const packageJson = readRootPackageJson() as ReturnType<typeof readRootPackageJson> & { scripts?: Record<string, string> }
   if (!packageJson.scripts) packageJson.scripts = {}
-  packageJson.scripts['nx-install'] = 'nx @enio.ai/nx-install'
+  packageJson.scripts['nx-install'] = 'nx affected --target=nx-install --all'
   writeJsonFile(join(workspaceRoot, 'package.json'), packageJson)
 }
