ce38d43dd69b3e7b18256bd09ddf4960ce9df2a3
Andrew Redican <andrew.redican.mejia@gmail.com>
2023-01-15 14:05:00 +0000

    feat(@badge-service): generate badge svg

--- apps/@badge-service/src/index.ts
+++ apps/@badge-service/src/index.ts
@@ -1,16 +1,18 @@
-import * as functions from 'firebase-functions'
+import { https } from 'firebase-functions'
 import * as process from './lib'
 
-export const createBuildBadge = functions.https.onRequest((request, response) => {
-  process.createBuildBadge(true)
-  response.send('Create build badge not fully implemented')
+export const createBuildBadge = https.onRequest((request, response) => {
+  const { passed } = request.params
+  const svg = process.createBuildBadge(passed == 'true')
+  response.send(svg)
 })
 
-export const createCodeCoverageBadge = functions.https.onRequest((request, response) => {
-  process.createCodeCoverageBadge('100', '50', '80')
-  response.send('Create coverage badge not fully implemented')
+export const createCodeCoverageBadge = https.onRequest((request, response) => {
+  const { percentage, minThreshold, maxThreshold } = request.params
+  const svg = process.createCodeCoverageBadge(percentage, minThreshold, maxThreshold)
+  response.send(svg)
 })
 
-export const getBadge = functions.https.onRequest((request, response) => {
+export const getBadge = https.onRequest((request, response) => {
   response.send('Get badge not fully implemented')
 })
