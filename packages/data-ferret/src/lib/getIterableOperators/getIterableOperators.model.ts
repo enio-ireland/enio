3403a6d43ae776085bd8082d547d9c0c062f6dd3
Andrew Redican <andrew.redican.mejia@gmail.com>
2023-01-22 16:57:50 +0000

    refactor(data-ferret): relax iterable operator types since these are dynamic at runtime

--- packages/data-ferret/src/lib/getIterableOperators/getIterableOperators.model.ts
+++ packages/data-ferret/src/lib/getIterableOperators/getIterableOperators.model.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export interface IterableOperators {
-  instantiate: () => unknown
+  instantiate: () => any
   getKeys: (target: any) => string[]
-  read: (target: any, key: unknown) => unknown
-  write: (instance: unknown, value: unknown, key?: unknown) => void
+  read: (target: any, key: any) => any
+  write: (instance: any, value: any, key?: any) => void
 }
