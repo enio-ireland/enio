455115df3bdba87e4bc3f43a3e8c5642e98ee296
Andrew Redican <andrew.redican.mejia@gmail.com>
2023-01-24 02:34:14 +0000

    feat(data-ferret): add getUniqueKeys to API :sparkles:

--- packages/data-ferret/src/lib/getUniqueKeys/getUniqueKeys.ts
+++ packages/data-ferret/src/lib/getUniqueKeys/getUniqueKeys.ts
@@ -5,11 +5,11 @@ import { isIterableType } from '../isIterableType/isIterableType'
 import { getIterableOperators } from '../getIterableOperators/getIterableOperators'
 
 /**
- * Returns a list of locations where the key name matches a pattern or an exact value anywhere in the data structure of the target.
+ * Returns a list of unique key names that match a pattern or an exact value anywhere in the data structure of the target.
  * A depth option is available to narrow down the iteration scope.
  * It supports other iterable data types, provided these have been made known using [registerIterableClass](https://github.com/enio-ireland/enio/tree/develop/packages/data-ferret/src/lib/registerIterableClass).
  **/
-export const locateKey = (target: unknown, pattern: string | RegExp, options?: Options): string[][] => {
+export const getUniqueKeys = (target: unknown, pattern: string | RegExp = /(.|\s)*/, options?: Options): string[] => {
   const patternIsString = typeof pattern === 'string'
   if (!patternIsString && !(pattern instanceof RegExp)) throw new Error('Expected pattern to be either a string of a regular expression.')
   const match = patternIsString ? (key: string) => key === pattern : (key: string) => pattern.test(key)
@@ -17,7 +17,7 @@ export const locateKey = (target: unknown, pattern: string | RegExp, options?: O
     const type = getType(value)
     if (!isIterableType(type)) return
     const { getKeys } = getIterableOperators(type)
-    getKeys(value).forEach(nextKey => match(nextKey) && state.locations.push([...path, nextKey]))
+    getKeys(value).forEach(nextKey => match(nextKey) && state.names.add(nextKey))
   }
-  return traverse(target, callback, { depth: [0, '*'], ...options } as Options, { locations: [] }).locations
+  return Array.from(traverse(target, callback, { depth: [0, '*'], ...options } as Options, { names: new Set<string>() }).names.values())
 }
