cf4a7dd343593ff52be0b173d3dad951cd9e5395
Andrew Redican <andrew.redican.mejia@gmail.com>
2023-02-12 02:12:16 +0000

    refactor(firebase): use path-schema to get database paths

---
+++ apps/firebase/src/shared/database/database.ts
@@ -0,0 +1,14 @@
+import { getPaths } from '@enio.ai/path-schema'
+import type { DB } from './database.model'
+
+const schema = `
+  @root » #projects
+  #projects » #opensource #enterprise
+  #enterprise 'enterprise' enterprise/[projectName] » #badges
+  #opensource 'openSource' opensource/[projectName] » #badges
+  #badges 'badges' badge » #build #coverage
+  #build 'buildPass' build
+  #coverage 'codeCoverage' coverage
+`
+
+export const db = getPaths<DB>(schema)
---
+++ apps/firebase/src/shared/index.ts
@@ -0,0 +1,1 @@
+export { db } from './database/database'
