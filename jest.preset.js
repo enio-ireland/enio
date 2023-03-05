fde8e93e4c48c99ee25c27084cbaa41e60e30ca5
Andrew Redican <andrew.redican.mejia@gmail.com>
2023-03-05 12:17:58 +0000

    chore: update dependencies :arrow_up:

--- jest.preset.js
+++ jest.preset.js
@@ -12,5 +12,15 @@ module.exports = {
   },
   collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/node_modules/**', '!**/*.spec.*', '!**/*.config.*', '!**/index.*'],
   coverageReporters: ['lcov', 'text', 'json-summary'],
-  reporters: ['default', 'github-actions']
+  reporters: ['default', 'github-actions'],
+  /* TODO: Update to latest Jest snapshotFormat
+   * By default Nx has kept the older style of Jest Snapshot formats
+   * to prevent breaking of any existing tests with snapshots.
+   * It's recommend you update to the latest format.
+   * You can do this by removing snapshotFormat property
+   * and running tests with --update-snapshot flag.
+   * Example: "nx affected --targets=test --update-snapshot"
+   * More info: https://jestjs.io/docs/upgrading-to-jest29#snapshot-format
+   */
+  snapshotFormat: { escapeString: true, printBasicPrototype: true }
 }
